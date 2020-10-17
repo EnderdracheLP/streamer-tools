ddfee98186db86f812e26f1ff05ceb4c1677666d
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-10-17 06:31:12 +0100

    Ignored some unneeded files

--- include/mod_interface.hpp
+++
@@ -1,2 +0,0 @@
-// Place only forward declarations of functions that you intend other mods to be able to use here.
-// That way you can share only this file and the compiled .so and they can link with it.
