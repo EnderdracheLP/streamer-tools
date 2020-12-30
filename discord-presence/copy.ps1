3bc1aeddf464cb360003311e92c621d339463f14
Laurie Fay <lauriethefish@outlook.com>
2020-12-30 11:22:03 +0000

    Updated copy path.

--- discord-presence/copy.ps1
+++ discord-presence/copy.ps1
@@ -6,5 +6,5 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 }
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-& adb push libs/arm64-v8a/libdiscord-presence_0_1_3.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libdiscord-presence_0_1_3.so
+& adb push libs/arm64-v8a/libdiscord-presence.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libdiscord-presence.so
 & adb shell am force-stop com.beatgames.beatsaber
