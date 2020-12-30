47664c2d83dee931d27e7400d071d10b8a87f88f
Laurie Fay <lauriethefish@outlook.com>
2020-12-30 11:44:04 +0000

    Version 0.2.0

--- discord-presence/buildBMBF.ps1
+++ discord-presence/buildBMBF.ps1
@@ -7,4 +7,4 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 }
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libdiscord-presence.so", "cover.png", "./bmbfmod.json", "module.json", "./extern/libbeatsaber-hook_0_8_4.so" -DestinationPath "./discord-presence_v0.1.3.zip" -Update
+Compress-Archive -Path "./libs/arm64-v8a/libdiscord-presence.so", "cover.png", "./bmbfmod.json", "module.json" -DestinationPath "./discord-presence_v0.2.0.zip" -Update
