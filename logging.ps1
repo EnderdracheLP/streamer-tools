9c2dea293e89a9f61f54d87db491ae011b9c5603
EnderdracheLP <enderdrachelp@gmail.com>
2021-06-22 20:48:31 +0200

    Updated logging script
    - Added `--stack` and `--stack-all` options that either get the crashes of the last timestamp set by the last time logging was executed or get use ndk-stack on all crash logs in adb logcat buffer
    - logging script will now auto evaluate all crashes that where logged upon script cancel with Ctrl + C

--- logging.ps1
+++ logging.ps1
@@ -1,3 +1,50 @@
+$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+$ndkstackScript = "$NDKPath/ndk-stack"
+
+if (-not ($PSVersionTable.PSEdition -eq "Core")) {
+    $ndkstackScript += ".cmd"
+}
+
+if ($args[0] -eq "--stack") {
+$timestamp = Get-Content $PSScriptRoot/log_timestamp
+#& $ndkstackScript -sym ./obj/local/arm64-v8a/ -dump ./logcat.log | Tee-Object -FilePath $PSScriptRoot/log_unstripped.log
+adb logcat -b crash -d -T "$timestamp" | & $ndkstackScript -sym ./obj/local/arm64-v8a/ | Tee-Object -FilePath $PSScriptRoot/log_unstripped.log
+break Script
+}
+
+if ($args[0] -eq "--stack-all") {
+#& $ndkstackScript -sym ./obj/local/arm64-v8a/ -dump ./logcat.log | Tee-Object -FilePath $PSScriptRoot/log_unstripped.log
+adb logcat -b crash -d | & $ndkstackScript -sym ./obj/local/arm64-v8a/ | Tee-Object -FilePath $PSScriptRoot/log_unstripped.log
+break Script
+}
+
 $timestamp = Get-Date -Format "MM-dd HH:mm:ss.fff"
-adb logcat -T "$timestamp" main-modloader:W libmodloader:W libmain:W QuestHook[streamer-tools`|v0.1.0-InDev]:* QuestHook[streamer-tools`|v0.1.0-Dev]:* AndroidRuntime:E *:S QuestHook[UtilsLogger`|v1.3.5]:E | Tee-Object -FilePath $PSScriptRoot/logcat.log
-echo "Exiting logging.ps1"
+$logger = Start-Job -Arg "$timestamp", $PSScriptRoot -scriptblock {
+    param($timestamp, $Path)
+    $timestamp > log_timestamp
+    adb logcat -v color -T "$timestamp" main-modloader:W libmodloader:W libmain:W QuestHook[streamer-tools`|v0.1.0-InDev]:* QuestHook[streamer-tools`|v0.1.0-Dev]:* AndroidRuntime:E *:S QuestHook[UtilsLogger`|v1.3.5]:E | Tee-Object -FilePath "$Path/logcat.log"
+}
+[console]::TreatControlCAsInput = $true
+while ($true)
+{
+    $buffer = Receive-Job -Job $logger
+    $PermaBuffer += $buffer
+    echo $buffer
+    if ([console]::KeyAvailable)
+    {
+        $key = [system.console]::readkey($true)
+        if (($key.modifiers -band [consolemodifiers]"control") -and ($key.key -eq "C"))
+        {
+            Add-Type -AssemblyName System.Windows.Forms
+            if ([System.Windows.Forms.MessageBox]::Show("Are you sure you want to exit?", "Exit Script?", [System.Windows.Forms.MessageBoxButtons]::YesNo) -eq "Yes")
+            {
+                echo "Terminating logcat and getting crash dumps..."
+                get-job | remove-job -Force
+                adb logcat -b crash -d -T "$timestamp" | & $ndkstackScript -sym ./obj/local/arm64-v8a/ | Tee-Object -FilePath $PSScriptRoot/log_unstripped.log
+                echo "Exiting logging.ps1"
+                break
+            }
+        }
+    }
+}
+
