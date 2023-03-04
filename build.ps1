ef6450b636951524d5b105595edd59bb873dc239
kodenamekrak <kyelodge2005@gmail.com>
2023-03-04 12:22:21 +0000

    1.28 and remove old files

--- build.ps1
+++ build.ps1
@@ -1,59 +1,31 @@
-Param (
-[Parameter(Mandatory=$false, HelpMessage="The version the mod should be compiled with")][Alias("ver")][string]$Version,
-[Parameter(Mandatory=$false, HelpMessage="Switch to create a clean compilation")][Alias("rebuild")][Switch]$clean,
-[Parameter(Mandatory=$false, HelpMessage="To create a release build")][Alias("publish")][Switch]$release,
-[Parameter(Mandatory=$false, HelpMessage="To create a github actions build, assumes specific Environment variables are set")][Alias("github-build")][Switch]$actions
+Param(
+    [Parameter(Mandatory=$false)]
+    [Switch] $clean,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $help
 )
-$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
-$QPMpackage = "./qpm.json"
-$qpmjson = Get-Content $QPMpackage -Raw | ConvertFrom-Json
-$ModID = $qpmjson.info.id
-if (-not $Version) {
-    $VERSION = $qpmjson.info.version
-} else {
-    $VERSION = $Version
-}
-if ($release -ne $true -and -not $VERSION.Contains('-Dev')) {
-    $VERSION += "-Dev"
+
+if ($help -eq $true) {
+    Write-Output "`"Build`" - Copiles your mod into a `".so`" or a `".a`" library"
+    Write-Output "`n-- Arguments --`n"
+
+    Write-Output "-Clean `t`t Deletes the `"build`" folder, so that the entire library is rebuilt"
+
+    exit
 }
 
-if ($env:version -eq "") {
-    & qpm-rust package edit --version $VERSION
-}
-
-if ((Test-Path "./extern/includes/beatsaber-hook/src/inline-hook/And64InlineHook.cpp", "./extern/includes/beatsaber-hook/src/inline-hook/inlineHook.c", "./extern/includes/beatsaber-hook/src/inline-hook/relocate.c") -contains $false) {
-    Write-Host "Critical: Missing inline-hook"
-    if (!(Test-Path "./extern/includes/beatsaber-hook/src/inline-hook/And64InlineHook.cpp")) {
-        Write-Host "./extern/includes/beatsaber-hook/src/inline-hook/And64InlineHook.cpp"
-    }
-    if (!(Test-Path "./extern/includes/beatsaber-hook/src/inline-hook/inlineHook.c")) {
-        Write-Host "./extern/includes/beatsaber-hook/src/inline-hook/inlineHook.c"
-    }
-        if (!(Test-Path "./extern/includes/beatsaber-hook/inline-hook/src/relocate.c")) {
-        Write-Host "./extern/includes/beatsaber-hook/src/inline-hook/relocate.c"
-    }
-    Write-Host "Task Failed, see output above"
-    exit 1;
-}
-echo "Building mod with ModID: $ModID version: $VERSION"
-
-if ($clean.IsPresent)
-{
-    if (Test-Path -Path "build")
-    {
+# if user specified clean, remove all build files
+if ($clean.IsPresent) {
+    if (Test-Path -Path "build") {
         remove-item build -R
     }
 }
 
-if (($clean.IsPresent) -or (-not (Test-Path -Path "build")))
-{
-    $out = new-item -Path build -ItemType Directory
-}
 
-cd build
-& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" ../
-& cmake --build . -j 6
-$ExitCode = $LastExitCode
-cd ..
-exit $ExitCode
-echo Done
+if (($clean.IsPresent) -or (-not (Test-Path -Path "build"))) {
+    new-item -Path build -ItemType Directory
+} 
+
+& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" -B build
+& cmake --build ./build
--- copy.ps1
+++ copy.ps1
@@ -1,21 +1,70 @@
-if ($args[0] -eq "--debug" -or $args[1] -eq "--debug") {
-& $PSScriptRoot/build.ps1 --debug
-} else {
-& $PSScriptRoot/build.ps1
+Param(
+    [Parameter(Mandatory=$false)]
+    [Switch] $clean,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $log,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $useDebug,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $self,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $all,
+
+    [Parameter(Mandatory=$false)]
+    [String] $custom="",
+
+    [Parameter(Mandatory=$false)]
+    [String] $file="",
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $help
+)
+
+if ($help -eq $true) {
+    Write-Output "`"Copy`" - Builds and copies your mod to your quest, and also starts Beat Saber with optional logging"
+    Write-Output "`n-- Arguments --`n"
+
+    Write-Output "-Clean `t`t Performs a clean build (equvilant to running `"build -clean`")"
+    Write-Output "-UseDebug `t Copies the debug version of the mod to your quest"
+    Write-Output "-Log `t`t Logs Beat Saber using the `"Start-Logging`" command"
+
+    Write-Output "`n-- Logging Arguments --`n"
+
+    & $PSScriptRoot/start-logging.ps1 -help -excludeHeader
+
+    exit
 }
-if ($?) {
-    adb push build/libStreamer-Tools.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libStreamer-Tools.so
-    if ($?) {
-        adb shell am force-stop com.beatgames.beatsaber
-        adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
-        if ($args[0] -eq "--log" -or $args[1] -eq "--log") {
-            $timestamp = Get-Date -Format "MM-dd HH:mm:ss.fff"
-            Start-Process PowerShell -ArgumentList "./logging.ps1 --file"
-        }
-        if ($args[0] -eq "--debug-log" -or $args[1] -eq "--debug-log") {
-            $timestamp = Get-Date -Format "MM-dd HH:mm:ss.fff"
-            adb logcat -T "$timestamp" main-modloader:W QuestHook[streamer-tools`|v0.1.0]:* AndroidRuntime:E *:S QuestHook[UtilsLogger`|v1.2.4]:*
-        }
+
+& $PSScriptRoot/build.ps1 -clean:$clean
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Output "Failed to build, exiting..."
+    exit $LASTEXITCODE
+}
+
+# & $PSScriptRoot/validate-modjson.ps1
+# if ($LASTEXITCODE -ne 0) {
+#     exit $LASTEXITCODE
+# }
+$modJson = Get-Content "./mod.json" -Raw | ConvertFrom-Json
+
+$modFiles = $modJson.modFiles
+
+foreach ($fileName in $modFiles) {
+    if ($useDebug -eq $true) {
+        & adb push build/debug/$fileName /sdcard/Android/data/com.beatgames.beatsaber/files/mods/$fileName
+    } else {
+        & adb push build/$fileName /sdcard/Android/data/com.beatgames.beatsaber/files/mods/$fileName
     }
 }
-echo "Exiting Copy.ps1"
+
+& $PSScriptRoot/restart-game.ps1
+
+if ($log -eq $true) {
+    & adb logcat -c
+    & $PSScriptRoot/start-logging.ps1 -self:$self -all:$all -custom:$custom -file:$file
+}
